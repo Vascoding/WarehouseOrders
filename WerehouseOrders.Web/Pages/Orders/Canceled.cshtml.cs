ed629e389e844d9eadfef4ed035a7dfa6c2607bb
sasholilov <alex_xld@abv.bg>
2020-10-16 15:19:46 +0300

    Added canceled orders

---
+++ WerehouseOrders.Web/Pages/Orders/Canceled.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WerehouseOrders.Models.Enums;
+using WerehouseOrders.Models.View.Filter;
+using WerehouseOrders.Services.Contracts;
+using WerehouseOrders.Web.Pages.Abstractions.Orders;
+
+namespace WerehouseOrders.Web.Pages.Orders
+{
+    [BindProperties]
+    public class CanceledModel : ListingOrderModel
+    {
+        public CanceledModel(IEntityService entityService, IMappingService mapper)
+            : base(entityService, mapper) { }
+
+        public override async Task OnGet(OrdersFilterModel filter, int currentPage = 1)
+        {
+            filter.Status = Status.Refused;
+
+            await base.OnGet(filter, currentPage);
+        }
+    }
+}
